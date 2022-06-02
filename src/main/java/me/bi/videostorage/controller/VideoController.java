021f68c4fd362074887030272bd83a0910d5e7e6
“rotomoo” <quddlf410@naver.com>
2022-06-02 17:46:19 +0900

    fix : except VideoController class

--- src/main/java/me/bi/videostorage/controller/VideoController.java
+++ src/main/java/me/bi/videostorage/controller/VideoController.java
@@ -16,45 +16,45 @@ import java.net.MalformedURLException;
 @RestController
 public class VideoController {
 
-    @Value("${video.location}")
-    private String videoLocation;
-
-    @GetMapping("/videos/{name}/full")
-    public ResponseEntity<UrlResource> getFullVideo(@PathVariable String name) throws MalformedURLException {
-        UrlResource video = new UrlResource("file:${video.location}/${name}");
-        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
-                .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
-                .body(video);
-    }
-
-    @GetMapping("/videos/{name}")
-    public ResponseEntity<ResourceRegion> getVideo(@PathVariable String name,
-                                                   @RequestHeader HttpHeaders headers) throws IOException {
-
-        log.info("getVideo");
-
-        UrlResource video = new UrlResource("classpath:" + videoLocation + "/" + name);
-        ResourceRegion region = resourceRegion(video, headers);
-        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
-                .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
-                .body(region);
-    }
-
-    private ResourceRegion resourceRegion(UrlResource video, HttpHeaders headers) throws IOException {
-
-        final long chunkSize = 1000000L;
-        long contentLength = video.contentLength();
-
-        HttpRange httpRange = headers.getRange().stream().findFirst().get();
-        if(httpRange != null) {
-            long start = httpRange.getRangeStart(contentLength);
-            long end = httpRange.getRangeEnd(contentLength);
-            long rangeLength = Long.min(chunkSize, end - start + 1);
-            return new ResourceRegion(video, start, rangeLength);
-        } else {
-            long rangeLength = Long.min(chunkSize, contentLength);
-            return new ResourceRegion(video, 0, rangeLength);
-        }
-    }
+//    @Value("${video.location}")
+//    private String videoLocation;
+//
+//    @GetMapping("/videos/{name}/full")
+//    public ResponseEntity<UrlResource> getFullVideo(@PathVariable String name) throws MalformedURLException {
+//        UrlResource video = new UrlResource("file:${video.location}/${name}");
+//        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+//                .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
+//                .body(video);
+//    }
+//
+//    @GetMapping("/videos/{name}")
+//    public ResponseEntity<ResourceRegion> getVideo(@PathVariable String name,
+//                                                   @RequestHeader HttpHeaders headers) throws IOException {
+//
+//        log.info("getVideo");
+//
+//        UrlResource video = new UrlResource("classpath:" + videoLocation + "/" + name);
+//        ResourceRegion region = resourceRegion(video, headers);
+//        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+//                .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
+//                .body(region);
+//    }
+//
+//    private ResourceRegion resourceRegion(UrlResource video, HttpHeaders headers) throws IOException {
+//
+//        final long chunkSize = 1000000L;
+//        long contentLength = video.contentLength();
+//
+//        HttpRange httpRange = headers.getRange().stream().findFirst().get();
+//        if(httpRange != null) {
+//            long start = httpRange.getRangeStart(contentLength);
+//            long end = httpRange.getRangeEnd(contentLength);
+//            long rangeLength = Long.min(chunkSize, end - start + 1);
+//            return new ResourceRegion(video, start, rangeLength);
+//        } else {
+//            long rangeLength = Long.min(chunkSize, contentLength);
+//            return new ResourceRegion(video, 0, rangeLength);
+//        }
+//    }
 
 }
