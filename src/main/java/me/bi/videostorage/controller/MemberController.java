adfa44c02e8fcad971dadf492aca5f2127b4ee9c
“rotomoo” <quddlf410@naver.com>
2022-06-23 12:35:45 +0900

    feat : logout, deleteMember method

--- src/main/java/me/bi/videostorage/controller/MemberController.java
+++ src/main/java/me/bi/videostorage/controller/MemberController.java
@@ -4,23 +4,41 @@ import lombok.RequiredArgsConstructor;
 import me.bi.videostorage.dto.MemberResponseDto;
 import me.bi.videostorage.dto.MemberUpdateDto;
 import me.bi.videostorage.service.MemberService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
+@PreAuthorize("isAuthenticated()")
 public class MemberController {
 
     private final MemberService memberService;
 
     @GetMapping("/me")
-    public ResponseEntity<MemberResponseDto> getCurrentInfo() {
+    public ResponseEntity<MemberResponseDto> getMyInfo() {
         return ResponseEntity.ok(memberService.getMyInfo());
     }
 
     @PutMapping("/update")
-    public void updateMember(@RequestBody MemberUpdateDto dto) {
+    public void updateMyInfo(@RequestBody MemberUpdateDto dto) {
         memberService.updateMyInfo(dto);
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<String> deleteMember(HttpServletRequest request) {
+        memberService.logout(request);
+        memberService.deleteMember();
+        return new ResponseEntity<>("회원 탈퇴 성공", HttpStatus.OK);
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request) {
+        memberService.logout(request);
+        return new ResponseEntity<>("로그아웃 성공", HttpStatus.OK);
     }
 }
