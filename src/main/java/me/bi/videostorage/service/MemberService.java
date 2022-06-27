f31d8d5afda8858e7b656d37458770d19f52b328
“rotomoo” <quddlf410@naver.com>
2022-06-27 14:19:44 +0900

    refactor : update MemberService

--- src/main/java/me/bi/videostorage/service/MemberService.java
+++ src/main/java/me/bi/videostorage/service/MemberService.java
@@ -25,7 +25,9 @@ public class MemberService {
     private final RefreshTokenRepository refreshTokenRepository;
     private final StringRedisTemplate redisTemplate;
 
-    // 내 정보 조회
+    /**
+     * 내 정보 조회
+     */
     @Transactional(readOnly = true)
     public MemberResponseDto getMyInfo() {
         return memberRepository.findById(SecurityUtil.getCurrentMemberId())
@@ -33,7 +35,9 @@ public class MemberService {
                 .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
     }
 
-    // 내 정보 수정 (이메일 수정 불가)
+    /**
+     * 내 정보 수정 (이메일 수정 불가)
+     */
     @Transactional
     public void updateMyInfo(MemberUpdateDto dto) {
         Member member = memberRepository
@@ -43,7 +47,9 @@ public class MemberService {
         member.updateMember(dto,passwordEncoder);
     }
 
-    // 로그아웃
+    /**
+     * 로그아웃
+     */
     @Transactional
     public void logout(HttpServletRequest request) {
 
@@ -57,7 +63,9 @@ public class MemberService {
                 .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
     }
 
-    // 회원 탈퇴
+    /**
+     * 회원 탈퇴
+     */
     @Transactional
     public void deleteMember() {
         memberRepository.deleteByEmail(getMyInfo().getEmail())
