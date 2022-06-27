908458fb76c361b5f7a3dbb1d25a5c23a6260cb9
“rotomoo” <quddlf410@naver.com>
2022-06-27 12:14:20 +0900

    feat : uploadVideo, playVideo, playVideoTest, findVideos method

---
+++ src/main/java/me/bi/videostorage/dto/VideoPlayRequestDto.java
@@ -0,0 +1,9 @@
+package me.bi.videostorage.dto;
+
+import lombok.Data;
+
+@Data
+public class VideoPlayRequestDto {
+
+    private String file;
+}
--- src/main/java/me/bi/videostorage/repository/VideoRepository.java
+++ src/main/java/me/bi/videostorage/repository/VideoRepository.java
@@ -1,7 +1,11 @@
 package me.bi.videostorage.repository;
 
+import me.bi.videostorage.domain.RefreshToken;
 import me.bi.videostorage.domain.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VideoRepository extends JpaRepository<Video, Long> {
+    Optional<Video> findByOrigFileName(String origFileName);
 }
