d2235842c698daefbf2300f84d2d499089818e60
“rotomoo” <quddlf410@naver.com>
2022-06-27 21:47:44 +0900

    refactor : update dto

--- src/main/java/me/bi/videostorage/dto/VideoPlayRequestDto.java
+++ src/main/java/me/bi/videostorage/dto/VideoPlayRequestDto.java
@@ -1,10 +1,8 @@
 package me.bi.videostorage.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class VideoPlayRequestDto {
 
     private String file;
